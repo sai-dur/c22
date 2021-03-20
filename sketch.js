1f4e6bc76ceaf1a54fb3a95094c44b11aea37898
sai-dur <76192029+sai-dur@users.noreply.github.com>
2021-03-20 15:40:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,67 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine, world;
-var ground;
-
-function setup(){
-    var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
-
-    var ground_options ={
-        isStatic: true
-    }
-
-   ground= Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
-var ball_options={
-    'restitution':1.0
-}
-   ball=Bodies.rectangle(50,100,50,50,ball_options)
-   World.add(world,ball)
-
-   var ball1_options={
-    'restitution':1.0
-   }
-ball1=Bodies.circle(100,100,30,ball1_options)
-World.add(world,ball1)
-    console.log(ground);
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,30);
-    ellipse(ball1.position.x,ball1.position.y,30,30)
-    rect(ball.position.x,ball.position.y,40,40);
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
 }
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x = packageBody.position.x 
+  packageSprite.y = packageBody.position.y 
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false)
+
+	}
+}
+
+
+
